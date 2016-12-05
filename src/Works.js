ce6c87f2dad148587da3154fb5815d3b996e93bc
killing333 <hlwanhoj@gmail.com>
2016-12-06 00:34:32 +0800

    Updated layout

--- src/Works.js
+++ src/Works.js
@@ -3,7 +3,10 @@ import React from 'react';
 class Work extends React.Component {
     render( ) {
         return (
-            <li>
+            <li id={this.props.id}>
+                <div className="bg">
+                    <div className="overlay"></div>
+                </div>
                 <h3>
                     <a href={this.props.URL}>{this.props.name}</a>
                 </h3>
@@ -16,7 +19,7 @@ class Works extends React.Component {
     render( ) {
         var rows = [ ];
         this.props.works.forEach( function( work ) {
-            rows.push( <Work key={work.URL} name={work.name} URL={work.URL} description={work.description}/> );
+            rows.push( <Work key={work.id} id={work.id} name={work.name} URL={work.URL} description={work.description}/> );
         });
         return (
             <ul>{rows}</ul>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './css/main.css';
 
 const works = [
     {
+        id: "mark-six-checker",
         name: "Mark Six Checker",
         URL: "https://killing333.github.io/Mark-Six-Checker",
         description: "Check the mark six number in an efficient way."
